878cf1ee52b8f98518c4571fe0129c995b14cd38
Faith Kariuki <tcwamboi@gmail.com>
2024-09-05 14:02:15 +0300

    complete lab

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,66 @@
 class Author:
-    pass
+    all = []
+    
+    def __init__(self, name):
+        if isinstance(name, str):
+            self.name = name
+        else:
+            raise Exception("Name must be a string")
+        Author.all.append(self)
+    
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.author == self]
+    
+    def books(self):
+        return [contract.book for contract in Contract.all if contract.author == self]
+    
+    def sign_contract(self, book, date, royalties):
+        contract = Contract(self, book, date, royalties)
+        return contract
+
+    def total_royalties(self):
+        return sum(contract.royalties for contract in self.contracts())
 
 
 class Book:
-    pass
+    all = []
+    
+    def __init__(self, title):
+        if isinstance(title, str):
+            self.title = title
+        else:
+            raise Exception("Title must be a string")
+        Book.all.append(self)
+        
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.book == self]
+    
+    def authors(self):
+        return [author for author in Author.all if author.book == self]
 
 
 class Contract:
-    pass
+    all = []
+    
+    def __init__(self, author, book, date, royalties):
+        if isinstance(author, Author):
+            self.author = author
+        else:
+            raise Exception("Author must be an instance of Autot class")
+        if isinstance(book, Book):
+            self.book = book
+        else:
+            raise Exception("Book must be an instance of Book class")
+        if isinstance(date, str):
+            self.date = date
+        else:
+            raise Exception("Date must be a string")
+        if isinstance(royalties, int):
+            self.royalties = royalties
+        else:
+            raise Exception("Royalties must be an integer")
+        Contract.all.append(self)
+    
+    @classmethod
+    def contracts_by_date(cls, date):
+        return [contract for contract in Contract.all if contract.date == date]
